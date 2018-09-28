22b2e1be90e72f29390d55385325712e83c8f500
Tilman Kamp <5991088+tilmankamp@users.noreply.github.com>
2018-09-28 13:13:56 +0200

    Optional httpfs dependency

--- src/pit.js
+++ src/pit.js
@@ -3,7 +3,6 @@ const fs = require('fs')
 const os = require('os')
 const tmp = require('tmp')
 const path = require('path')
-const httpfs = require('httpfs')
 const program = require('commander')
 const request = require('request')
 const readlineSync = require('readline-sync')
@@ -944,6 +943,19 @@ program
         printLine('Home and group directories are write-enabled, all others are read-only.')
     })
     .action((entity, mountpoint, options) => {
+        let httpfs
+        try {
+            httpfs = require('httpfs')
+        } catch (ex) {
+            fail(
+                'For mounting, package "httpfs" has to be installed.\n' +
+                'Most likely it has been skipped due to missing dependencies.\n' +
+                'Please consult the following page for system specific requirements:\n' +
+                '\thttps://github.com/mafintosh/fuse-bindings#requirements\n' +
+                'Once fulfilled, you can either re-install snakepit-client or\n' +
+                'call again "yarn install" or "npm install" within its project root.'
+            )
+        }
         let endpoint
         entity = parseEntity(entity)
         if (entity.type == 'home') {
